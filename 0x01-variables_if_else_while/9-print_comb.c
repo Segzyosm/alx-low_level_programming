7e39638851264b91b75bd47e47ae7146f4736e26
Michael <segzy7@gmail.com>
2022-12-12 13:04:35 -0800

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints combinatination of all one digit numbers.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+
+{
+
+	int num;
+
+	for (numbe = 0; num <= 9; num++)
+
+	{
+	
+		putchar(num + '0');
+
+		if (num != 9)
+
+		{
+			putchar(',');
+
+			putchar(' ');
+
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
